d4e5520354ef4ba530b404c13c2e3e45071ffe71
Raymond Cavalcante <rcavalca@umich.edu>
2023-08-21 11:30:22 -0600

    Add QC parts of epi pipelines; update references and fix Rmd title block for references

--- knit_site.R
+++ knit_site.R
@@ -2,5 +2,10 @@ library(rmarkdown)
 
 render_site('source/index.md')
 render_site('source/bulk-rna-seq.Rmd')
+render_site('source/bulk-atac-seq.Rmd')
+render_site('source/bulk-chip-seq.Rmd')
+render_site('source/bulk-cut-and.Rmd')
+render_site('source/dna-methylation-epic.Rmd')
+render_site('source/dna-methylation-seq.Rmd')
 
 #clean_site(preview=TRUE)
