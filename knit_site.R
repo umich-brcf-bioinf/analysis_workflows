fadcda24c017d2fb84c9d54b33ccc0cc2b91f4ad
Chris Gates <cgates@umich.edu>
2023-08-29 15:33:11 -0400

    Added spatial and ability to generate all_methods word doc

--- knit_site.R
+++ knit_site.R
@@ -1,11 +1,18 @@
 library(rmarkdown)
 
+all_methods=FALSE
 render_site('source/index.md')
 render_site('source/bulk-rna-seq.Rmd')
+render_site('source/scsn-rna-seq.Rmd')
 render_site('source/bulk-atac-seq.Rmd')
 render_site('source/bulk-chip-seq.Rmd')
 render_site('source/bulk-cut-and.Rmd')
 render_site('source/dna-methylation-epic.Rmd')
 render_site('source/dna-methylation-seq.Rmd')
+render_site('source/spatial-GeoMX.Rmd')
+
 
 #clean_site(preview=TRUE)
+
+all_methods=TRUE
+rmarkdown::render('source/all_methods.Rmd')
